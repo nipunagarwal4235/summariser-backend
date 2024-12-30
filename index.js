b2430e5ba1a39b8abfaa6e2e2f9bf4e6a8782291
Nipun Agarwal <nipunagarwal322@gmail.com>
2024-12-31 01:10:08 +0530

    remove container fix

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ app.use(
 );
 
 // API Endpoints
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/summarise", summariseRouter);
